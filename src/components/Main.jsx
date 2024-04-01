bba1d46110ddebfdf8ceb385644108c280abd87a
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2024-04-01 10:50:32 -0400

    label e input para um novo estado

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,20 +2,29 @@
 
  export default function Main(){
     const [nome, setNome] = useState("");  //primeira:acessa  setNome:modifica
-    const [telefone, setTelefone] = useState("");
-   const [listaContatos, setContatos] = useState([]);
+    const [telefone, setTelefone] = useState("");  //ou useState(0)
+   const [listaContatos, setListaContatos] = useState([]);
+   const [cpf, setCpf] = useState("");
 
-   const registrar = () => {
-    alert("deu certo!");
-   }
+   const registrar = (event) => {
+      event.preventDefault();
+    alert("Deu certo!");
+    setListaContatos([...listaContatos,
+      {
+         nomeSalvo: nome,
+         telefoneSalvo: telefone
+      }
 
-
+    ]);
+   };
+console.table(listaContatos);
 
 
 return(
 <main>
-
     <form>
+
+
     <label htmlFor="nome"> Nome:</label>
     <input type ="text"name="nome-contato"id="nome" value= {nome} onChange={  (event)  => setNome(event.target.value)}/>
     {nome}
@@ -24,11 +33,16 @@ return(
      <input type ="tel"name="nome-contato"id="telefone"value= {telefone} onChange={  (event)  => setTelefone(event.target.value)}/>
      {telefone}
 
+     <label htmlFor= "cpfusuário">CPF:</label>
+     <input type = "text"name="nome-contato"id="cpf"value= {cpf} onChange={  (event)  => setCpf(event.target.value)}/>
+     {cpf}
 
     <button id="botaoSalvar">Salvar</button>
-     </form>
-</main>
+   
 
+     </form>
+   
+</main>
 );
 }
 //onChange dispara uma função
